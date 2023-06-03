4f90d34ceff6c0b08f595489ba74102598986a9a
Theop101005 <124686359+Theop101005@users.noreply.github.com>
2023-06-03 15:47:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+import pygame
+from sys import exit
+from levels import *
+from game_elements import *
+
+pygame.font.init()
+font = pygame.font.SysFont("freesansbold.ttf", 18)
+
+pygame.init
+window_icon = pygame.image.load("blood-cells.png")
+pygame.display.set_icon(window_icon)
+
+# establishing a surface for the game to be displayed on
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# naming the surface
+pygame.display.set_caption("Grimcell")
+
+# creating a clock to base frame rate off of
+FPS = 60
+clock = pygame.time.Clock()
+
+'''
+    buttons created
+'''
+level_select_btn = Button("Level Select", 350, 80, 100, 30, screen)
+level_create_btn = Button("Level Create", 350, 130, 100, 30, screen)
+endless_btn = Button("Endless", 350, 180, 100, 30, screen)
+level_1_btn = Button("Level 1", 350, 130, 100, 50, screen)
+# this button will only appear if level_select_btn is clicked
+level_1_btn.display = False
+
+'''
+    floor and player
+    player will collide with the floor
+    look at player.update() method in player.py
+'''
+floor = pygame.Rect(0, SCREEN_HEIGHT*0.66, SCREEN_WIDTH, SCREEN_HEIGHT/2)
+player = Player(floor.x, floor.y)
+mouse = pygame.mouse.get_pos()
+
+
+def level_select():
+    PLAY_MOUSE_POS = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+
+
+def main_menu():
+    screen.fill('grey')
+
+    if level_select_btn.display:
+        level_select_btn.draw()
+    if level_create_btn.display:
+        level_create_btn.draw()
+    if endless_btn.display:
+        endless_btn.draw()
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if level_select_btn.check_click():
+                '''
+                    all buttons dissapear and the level_1_btn shows
+                '''
+                level_select_btn.display = False
+                level_create_btn.display = False
+                endless_btn.display = False
+                level_1_btn.display = True
+
+    if level_1_btn.display:
+        level_1_btn.draw()
+
+    if level_1_btn.check_click():
+        level_1_btn.display = False
+    
+    '''
+        draw the player and the floor
+        update the player
+    '''
+    if not level_1_btn.display and not level_select_btn.display:
+        pygame.draw.rect(screen, 'brown', floor)
+        player.update(floor)
+        player.draw(screen)
+    pygame.display.update()
+
+
+# main menu
+while True:
+    # This is to cap the frame rate of the game and stop any issues that would occur through high frame rates
+    clock.tick(FPS)
+    main_menu()
+    pygame.display.update()
